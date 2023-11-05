f7274becf720183505ecda2f11196248b2192c69
scacner <cacman14@gmail.com>
2023-11-05 02:40:20 -0500

    allow upgrade script to run as root

--- upgrade.sh
+++ upgrade.sh
@@ -18,15 +18,15 @@ if [ -f VERSION ]; then
 fi
 
 # Verify not running as root
-if [ "$EUID" -eq 0 ]; then
-    echo "ERROR: Running this as root will cause permission issues."
-    echo ""
-    echo "Please ensure your local user is in the docker group and run without sudo."
-    echo "   sudo usermod -aG docker \$USER"
-    echo "   $0"
-    echo ""
-    exit 1
-fi
+#if [ "$EUID" -eq 0 ]; then
+#    echo "ERROR: Running this as root will cause permission issues."
+#    echo ""
+#    echo "Please ensure your local user is in the docker group and run without sudo."
+#    echo "   sudo usermod -aG docker \$USER"
+#    echo "   $0"
+#    echo ""
+#    exit 1
+#fi
 
 # Windows Git Bash docker exec compatibility fix
 if type winpty > /dev/null 2>&1; then
